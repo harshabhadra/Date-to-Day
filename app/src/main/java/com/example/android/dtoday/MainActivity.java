063f4ac69edb6995e83f950c1e1510e6654ab3ea
harshabhadra <harshasharkey@gmail.com>
2018-08-05 21:47:17 +0530

    Add app and gradle file

---
+++ app/src/main/java/com/example/android/dtoday/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.android.dtoday;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+public class MainActivity extends AppCompatActivity {
+
+    int date, month, year;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    public void submit(View view) {
+
+        EditText eDate = (EditText) findViewById(R.id.date);
+        EditText eMonth = (EditText) findViewById(R.id.month);
+        EditText eYear = (EditText) findViewById(R.id.year);
+
+        date = Integer.parseInt(eDate.getText().toString());
+        month = (Integer.parseInt(eMonth.getText().toString()) - 1);
+        year = Integer.parseInt(eYear.getText().toString());
+
+        String day = calculateDay(date, month, year);
+        printDate(day);
+
+    }
+
+    private String calculateDay(int dd, int mm, int yyyy) {
+
+        Date date = (new GregorianCalendar(yyyy, mm, dd)).getTime();
+
+
+        SimpleDateFormat sdf = new SimpleDateFormat("EEEE");
+        String day = sdf.format(date);
+
+        return day;
+    }
+
+    private void printDate (String s){
+        TextView textView = (TextView) findViewById(R.id.printDate);
+        textView.setText(s);
+    }
+
+
+}
